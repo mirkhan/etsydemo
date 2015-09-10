ff366ecb623ec1342d70ea61192a234a3c894dd7
Mir Khan <mirkhan@gmail.com>
2015-09-10 15:04:22 -0500

    Second rollback

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,12 +1,12 @@
 class Listing < ActiveRecord::Base
 
-	if rails.env.development
+	if Rails.env.development?
 		has_attached_file :photo, styles: {medium: "200x>", thumb: "100x100>" }, default_url: "no_image.png"
 
 	else
 
 	 has_attached_file :photo, styles: {medium: "200x>", thumb: "100x100>" }, default_url: "no_image.png",
-  ##validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/,
+  #validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/,
   	 					:storage => :dropbox,
     					:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
     					:path => ":style/:id_:filename"
